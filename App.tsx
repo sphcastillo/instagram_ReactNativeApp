3e99d225dbae21267cc418e0c73affc79e1064d9
Sophia Castillo <sphcastillo@gmail.com>
2023-07-11 16:31:31 -0700

    Theme / Vector Icons / additions ~ vectors ~ for ios  & Android

--- App.tsx
+++ App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { View, Text } from "react-native"; 
+import colors from './src/theme/colors';
+import font from  './src/theme/fonts';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 const App = () => {
   return (
     <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1}}>
-      <Text>Hello World</Text>
+      <Text style={{ color: colors.primary, fontSize: font.size.lg}}>
+        Hello World!!!
+        <AntDesign name="stepforward" size={25}/>
+        </Text>
     </View>
   )
 }
---
+++ src/theme/fonts.ts
@@ -0,0 +1,19 @@
+const size =  {
+    xsmall: 10,
+    small: 12,
+    default: 14,
+    md: 16,
+    lg: 20,
+    xlg: 24,
+    xxlg: 28
+};
+
+const weight = {
+    full: '900',
+    semi: '600',
+    bold: 'bold',
+    normal: 'normal',
+    thin: '400'
+}
+
+export default {  size, weight };
