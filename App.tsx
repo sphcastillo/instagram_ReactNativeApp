eb41a78773c6acfafcbf4b638b2cfea17e2a1b66
Sophia Castillo <sphcastillo@gmail.com>
2023-07-11 11:41:39 -0700

    first commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from "react-native"; 
+
+const App = () => {
+  return (
+    <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1}}>
+      <Text>Hello World</Text>
+    </View>
+  )
+}
+
+export default App;
---
+++ src/assets/theme/color.ts
@@ -0,0 +1,10 @@
+const colors = {
+    primary: "#405DE6",
+
+    white: "#fff",
+    black: '#000',
+    gray: '#666666',
+    lightgrey: '#cccccc'
+}
+
+export default colors;
