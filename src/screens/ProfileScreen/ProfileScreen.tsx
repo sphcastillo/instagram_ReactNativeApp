eb32c999f97a93d6d794f67984f000ac55a05917
Sophia Castillo <sphcastillo@gmail.com>
2023-08-14 23:24:26 -0700

    Created ProfileScreen and Button component

---
+++ src/screens/ProfileScreen/ProfileScreen.tsx
@@ -0,0 +1,51 @@
+import { View, Text, Image } from 'react-native'
+import React from 'react';
+import styles from './styles';
+import user from "../../assets/data/user.json";
+import Button from '../../components/Button';
+
+const ProfileScreen = () => {
+  return (
+    <View style={styles.root}>
+        <View style={styles.headerRow}>
+            {/* Profile image */}
+            <Image 
+                source={{ uri: user.image }}
+                style={styles.avatar}
+            />
+            {/* Posts, followers, following number */}
+            <View style={styles.numberContainer}>
+                <Text style={styles.numberText}>98</Text>
+                <Text>Posts</Text>
+            </View>
+
+            <View style={styles.numberContainer}>
+                <Text style={styles.numberText}>198</Text>
+                <Text>Followers</Text>
+            </View>
+
+            <View style={styles.numberContainer}>
+                <Text style={styles.numberText}>298</Text>
+                <Text>Following</Text>
+            </View>
+        </View>
+
+        <Text style={styles.name}>{user.name}</Text>
+        <Text>{user.bio}</Text>
+
+        <View style={{flexDirection: 'row'}}>
+            <Button 
+                text='Edit Profile'
+                onPress={() => console.warn('On Edit Profile')}
+            />
+
+            <Button 
+                text='Another Button'
+                onPress={() => console.warn('On another button')}
+            />            
+        </View>
+    </View>
+  )
+}
+
+export default ProfileScreen;
